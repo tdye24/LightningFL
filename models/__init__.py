56d75c6ac1b9120470c4f5c04b313ff7522116fb
tdye24 <18365225454@163.com>
2021-07-06 13:45:56 +0800

    add fedavg cifar100

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,6 @@
 from .fedavg.mnist.MNIST import MNIST as FedAvg_MNIST
 from .fedavg.cifar10.CIFAR10 import CIFAR10 as FedAvg_CIFAR10
+from .fedavg.cifar100.CIFAR100 import CIFAR100 as FedAvg_CIFAR100
 
 from .fedsp.cifar10.CIFAR10 import CIFAR10 as FedSP_CIFAR10
 from .fedmc.cifar10.CIFAR10 import CIFAR10 as FedMC_CIFAR10
@@ -7,6 +8,7 @@ from .fedmc.cifar10.CIFAR10 import CIFAR10 as FedMC_CIFAR10
 __all__ = [
     'FedAvg_MNIST',
     'FedAvg_CIFAR10',
+    'FedAvg_CIFAR100',
     'FedMC_CIFAR10',
     'FedSP_CIFAR10'
 ]
